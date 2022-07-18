6484017accdbf693e55f524135e399bb58380429
“Athisankar” <athisankar@mcci.com>
2022-07-18 19:46:58 +0530

    #1: initial commit

---
+++ banking/contact.php
@@ -0,0 +1,60 @@
+<?php
+    include "header.php";
+    include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="contact_style.css">
+</head>
+<body>
+  <div class="flex-container-background">
+    <div class="flex-container-heading">
+      <h1 id="contact">Contact Us</h1>
+    </div>
+    <div class="flex-container" style="border-bottom: 0;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;">
+    <div class="flex-item">
+      <h1 id="sub-contact">Corporate Headquarters</h1>
+      <p id="sub-contact">
+        <br>MCCI Bank<br>
+        chennai <br>
+        India
+      </p>
+    </div>
+    <div class="flex-item">
+      <h1 id="sub-contact">General Contact</h1>
+      <p id="sub-contact">
+        Toll-Free: 9898878776<br>
+        Phone: 9597660372<br>
+        Email: athisankar@mcci.com
+      </p>
+    </div>
+  </div>
+  <div class="flex-container" style="border-top: 0;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;">
+  <div class="flex-item">
+    <h1 id="sub-contact">Customer Care (24x7)</h1>
+    <p id="sub-contact">
+      Toll-Free: 9876543210<br>
+      Phone: 9597660372<br>
+      Email: athisankar@mcci.com
+    </p>
+  </div>
+  <div class="flex-item">
+    <h1 id="sub-contact"> CUSTOMERS  QUERIS </h1>\
+    <p id="sub-contact">
+      If any queries  PLEASE CONTACT US !
+      <br>
+      <br>
+      <p id="sub-contact">
+      </p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
